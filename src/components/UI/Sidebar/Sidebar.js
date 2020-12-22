d8449ab6bb1c4d3f907a59ff27581cf15e118549
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-22 11:34:39 +0000

    Create list of playlists in the sidebar and add some additional styling

--- src/components/UI/Sidebar/Sidebar.js
+++ src/components/UI/Sidebar/Sidebar.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Sidebar.scss'
 import logo from '../../../assets/images/logo2.png'
 
-const Sidebar = ({ children }) => {
+const Sidebar = () => {
+  const [state, setState] = useState({
+    currentPlaylist: 'home',
+    playlists: {
+      home: null,
+      favourites: null
+    }
+  })
+
+  const playlists = Object.keys(state.playlists)
+
   return (
     <div className="Sidebar">
       <img src={logo} alt="logo" className="Sidebar__logo"/>
-      {children}
+      <div className="Sidebar__title">LIBRARY</div>
+      {
+        playlists.map(list =>
+          <li 
+            className={list === state.currentPlaylist ? "Sidebar__li Sidebar__li--active" : "Sidebar__li"}
+            key={list}
+            onClick={() => {
+              setState({ ...state, currentPlaylist: list})
+            }}>
+            {list}
+          </li>)
+      }
+      <div className="Sidebar__newplaylist">
+        <span style={{userSelect: 'none'}}>New playlist</span>
+      </div>
     </div>
   )
 }
