0bbe6231f9d06f18969646a3852d150597adeb60
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-23 07:51:04 +0000

    Create adding a playlist logic, style scrollbar

--- src/components/UI/Sidebar/Sidebar.js
+++ src/components/UI/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './Sidebar.scss'
 import logo from '../../../assets/images/logo2.png'
@@ -12,39 +12,57 @@ const Sidebar = () => {
     currentPlaylist: 'home',
     showModal: false,
     playlists: {
-      home: null,
-      hiphop: null,
-      rock: null,
-      drums: null,
-      favourites: null
+      home: new Set(),
+      hiphop: new Set(),
+      rock: new Set(),
+      favourites: new Set()
     }
   })
 
   const playlists = Object.keys(state.playlists)
+  const playlistRef = useRef(null)
+
+  const addPlaylist = (event) => {
+    event.preventDefault()
+    const list = playlistRef.current.value
+
+    setState({
+      ...state, 
+      showModal: false,
+      playlists: {...state.playlists, [list]: new Set()}
+    })
+  }
 
   return (
     <div className="Sidebar">
       <img src={logo} alt="logo" className="Sidebar__logo"/>
       <div className="Sidebar__title">LIBRARY</div>
-      {
-        playlists.map(list =>
-          <li 
-            className={list === state.currentPlaylist ? "Sidebar__li Sidebar__li--active" : "Sidebar__li"}
-            key={list}
-            onClick={() => {
-              setState({ ...state, currentPlaylist: list})
-            }}>
-            {list}
-          </li>)
-      }
+      <ul className="Sidebar__ul">
+        {
+          playlists.map(list =>
+            <li 
+              className={list === state.currentPlaylist ? "Sidebar__li Sidebar__li--active" : "Sidebar__li"}
+              key={list}
+              onClick={() => {
+                setState({ ...state, currentPlaylist: list})
+              }}>
+              {list}
+            </li>)
+        }
+      </ul>
       <div className="Sidebar__newplaylist">
         <img className="Sidebar__newplaylist__img" src={plus} alt="plus" onClick={() => setState({...state, showModal: true})}/>
         <span className="Sidebar__newplaylist__title">New playlist</span>
         <Modal show={state.showModal} close={() => setState({...state, showModal: false})}>
-          <form className="Sidebar__newplaylist__modal">
+          <form onSubmit={addPlaylist} className="Sidebar__newplaylist__modal">
             <div className="Sidebar__newplaylist__modal__title">NEW PLAYLIST</div>
             <div className="Sidebar__newplaylist__modal__content">
-              <input className="Sidebar__newplaylist__modal__content__input" type="text" placeholder="NAME" required/>
+              <input
+                className="Sidebar__newplaylist__modal__content__input"
+                type="text"
+                placeholder="NAME"
+                required
+                ref={playlistRef}/>
             </div>
             <Button type="submit" text="CREATE" styles="black"></Button>
           </form>
