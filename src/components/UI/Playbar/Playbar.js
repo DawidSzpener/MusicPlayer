87400c4648e7345cd8f7d9b22d277fd3a55dc8f8
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-26 20:57:32 +0000

    Build <Playbar /> mockup

--- src/components/UI/Playbar/Playbar.js
+++ src/components/UI/Playbar/Playbar.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 import './Playbar.scss'
 
-const Playbar = ({ children }) => {
+const Playbar = () => {
   return (
     <div className="Playbar">
-      {children}
+      <div className="Playbar__volume">
+        <span className="Playbar__volume__bar"><img src={null} alt="volume bar"/></span>
+        <input type="range" name="volumeBar" />
+      </div>
+      <div className="Playbar__controls">
+        <img src={null} alt="backward button" className="Playbar__controls__backward"/>
+        <img src={null} alt="play button" className="Playbar__controls__play"/>
+        <img src={null} alt="forward button" className="Playbar__controls__forward"/>
+      </div>
+      <div className="Playbar__progressBar">
+        <span>current</span>
+        <input type="range" name="progressBar"/>
+        <span>total</span>
+      </div>
+      <span className="Playbar__repeat"></span>
     </div>
   )
 }
